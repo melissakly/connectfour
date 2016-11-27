5b4ec9e5fd46eb132505286c8668ce577796567e
Tina Zheng <tinazheng@berkeley.edu>
2016-11-26 22:09:29 -0500

    Current user name variable

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -9,9 +9,7 @@ class UserController < ApplicationController
 
 	def view
 		@curr_user ||= User.find(session[:user_id]) if session[:user_id]
-		@curr_user_name
 		@games = Game.where(user_1: @curr_user).or(Game.where(user_2: @curr_user))
-		#@opponents = games.map {|game| Game.get_opponent(game, @curr_user_name)}
 
 		fb_token = @curr_user.oauth_token
 		graph = Koala::Facebook::API.new(fb_token)
